9605f14147a3d5be30079aa10c96312f5a3662c4
ujjwalu26 <ujjwal.u@iqlect.com>
2023-01-09 11:23:20 +0530

    Adding favicon

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" type="image/png" href="public/fav-icon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>BangDB Documentation</title>
         <meta
